11eb2e0504c5cbcdc63a2a01bf2e90deb5b4b1d6
Abhishek Anand <abhi1aa@outlook.com>
2025-04-19 19:16:03 -0700

    added set_envs file

---
+++ set_envs.sh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+
+export OPENAI_API_KEY=<open_ai_api_key>
+export LANGSMITH_PROJECT="coc"
+export LANGSMITH_ENDPOINT="https://api.smith.langchain.com"
+export LANGSMITH_API_KEY=<lang_smith_api_key>
+export LANGSMITH_TRACING=true
